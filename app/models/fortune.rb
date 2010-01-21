61ecb360b2b4e034e0dc1db1d0f06ae10e00ee87
cde <cdechauri@gmail.com>
2010-01-20 20:31:41 -0800

    more tests and model stuff too

---
+++ app/models/fortune.rb
@@ -0,0 +1,2 @@
+class Fortune < ActiveRecord::Base
+end
---
+++ db/migrate/20100121033842_create_fortunes.rb
@@ -0,0 +1,13 @@
+class CreateFortunes < ActiveRecord::Migration
+  def self.up
+    create_table :fortunes do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :fortunes
+  end
+end
